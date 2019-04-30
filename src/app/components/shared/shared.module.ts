fb931bc107667081c66f8ee2ecbedeee2d7e37e1
edgon <edgon85@gmail.com>
2019-04-29 18:06:02 -0600

    Componente navbar creado

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
    // CommonModule
    RouterModule
   ]
   , exports: [
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    NavbarComponent
   ]
 })
 export class SharedModule { }
