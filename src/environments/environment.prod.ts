6246bf2f28177d661c43a86e2abd634deea830ec
edgon <edgon85@gmail.com>
2019-05-01 17:19:59 -0600

    pre prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA26C0nI5nlfr1-iS3fZqUhoLguhck5ufc',
+    authDomain: 'heroes-app-22dc0.firebaseapp.com',
+    databaseURL: 'https://heroes-app-22dc0.firebaseio.com',
+    projectId: 'heroes-app-22dc0',
+    storageBucket: 'heroes-app-22dc0.appspot.com',
+    messagingSenderId: '369251404570'
+  }
 };
